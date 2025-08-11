42820c9de4279079629f023de44ef6a1a978035c
Andrea Debernardi <andrea@debbaweb.it>
2025-08-11 12:04:12 +0200

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/icons/site.webmanifest" />
+        <link rel="shortcut icon" href="/assets/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>
         <Navigation />
         {children}
