524a9333b3a1b0494e5876d6b60c3d6867ad3187
Andrea Debernardi <andrea@debbaweb.it>
2025-08-11 11:50:39 +0200

    added matomo tracking

--- components/MatomoAnalytics.tsx
+++ components/MatomoAnalytics.tsx
@@ -3,33 +3,30 @@
 import Script from 'next/script'
 
 export default function MatomoAnalytics() {
-  const matomoSiteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
-  const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL
+    const matomoSiteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
+    const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL
 
-  if (!matomoSiteId || !matomoUrl) {
-    return null
-  }
+    if (!matomoSiteId || !matomoUrl) {
+        return null
+    }
 
-  return (
-    <>
-      <Script
+    return <Script
         id="matomo-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="${matomoUrl}";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '${matomoSiteId}']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="${matomoUrl}";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', ${matomoSiteId}]);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
           `,
         }}
-      />
-    </>
-  )
+    />
 }
